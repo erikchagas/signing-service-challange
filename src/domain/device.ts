25ba893ed629a5f6ef38867cc84f9deee01976ea
Erik Silva <erikchagas00@gmail.com>
2023-04-02 20:24:44 -0300

    feat: add inteface for device entity

--- src/domain/device.ts
+++ src/domain/device.ts
@@ -1,1 +1,13 @@
 // TODO: signature device domain model ...
+
+export type IAlgorithm = "ECC" | "RSA";
+export interface IDevice {
+  uuid: string;
+  algorithm: IAlgorithm;
+  label?: string;
+  privateKey?: string;
+  publicKey: string;
+  signatureCounter: number;
+}
+
+export interface ITransaction {}
