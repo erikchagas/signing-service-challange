152f3f1dc95f9fd8aa4d9b4cdab58f7e70fac4d1
Erik Silva <erikchagas00@gmail.com>
2023-04-02 20:27:20 -0300

    feat: add post and get methods for device route

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,18 +1,58 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import crypto from "crypto";
+import bodyParser from "body-parser";
+import Database from "../persistence/database";
+import { IDevice } from "../domain/device";
+import generateKeyPair from "../crypto/generation";
 
 const server = express();
 
+const database = new Database();
+
 server.use(bodyParser.json());
 
-server.get('/health', (req, res) => {
+server.get("/health", (req, res) => {
   res.status(200);
-  res.send(JSON.stringify({
-    status: 'pass',
-    version: 'v1'
-  }));
+  res.send(
+    JSON.stringify({
+      status: "pass",
+      version: "v1",
+    })
+  );
 });
 
 // TODO: REST endpoints ...
+server.post("/device", async (req, res) => {
+  const { algorithm, label } = req.body;
+  const uuid = crypto.randomUUID();
+
+  const { publicKey, privateKey } = await generateKeyPair(algorithm);
+
+  const device = {
+    uuid,
+    algorithm,
+    publicKey,
+    privateKey,
+    label: label || "No Label",
+    signatureCounter: 0,
+  };
+
+  database.insert("device", device);
+
+  const response: IDevice = {
+    uuid: device.uuid,
+    publicKey: device.publicKey,
+    algorithm: device.algorithm,
+    label: device.label,
+    signatureCounter: device.signatureCounter,
+  };
+
+  res.json(response);
+});
+
+server.get("/device", async (req, res) => {
+  const devices = database.select("device");
+  res.json(devices);
+});
 
 export default server;
