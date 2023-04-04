2083ac0f4419132b026253a8844d3a3a87661f76
Erik Silva <erikchagas00@gmail.com>
2023-04-04 01:38:23 -0300

    feat: add transaction routes and validation for post device route

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,7 +3,10 @@ import crypto from "crypto";
 import bodyParser from "body-parser";
 import Database from "../persistence/database";
 import { IDevice } from "../domain/device";
+import { ITransaction } from "../domain/transaction";
 import generateKeyPair from "../crypto/generation";
+import signECC from "../crypto/ecdsa";
+import signRSA from "../crypto/rsa";
 
 const server = express();
 
@@ -26,15 +29,23 @@ server.post("/device", async (req, res) => {
   const { algorithm, label } = req.body;
   const uuid = crypto.randomUUID();
 
+  console.log("algorithm ", algorithm);
+
+  if (!algorithm) return res.status(404).json({ error: "No algorithm found" });
+
+  if (algorithm !== "ECC" && algorithm !== "RSA")
+    return res.status(404).json({ error: "No valid algorithm found" });
+
   const { publicKey, privateKey } = await generateKeyPair(algorithm);
 
-  const device = {
+  const device: IDevice = {
     uuid,
     algorithm,
     publicKey,
     privateKey,
     label: label || "No Label",
     signatureCounter: 0,
+    lastSignature: null,
   };
 
   database.insert("device", device);
@@ -51,8 +62,64 @@ server.post("/device", async (req, res) => {
 });
 
 server.get("/device", async (req, res) => {
-  const devices = database.select("device");
-  res.json(devices);
+  res.json(database.select("device"));
+});
+
+server.post("/transaction/:deviceUuid", async (req, res) => {
+  const { deviceUuid } = req.params;
+  const { dataToBeSigned } = req.body;
+
+  const uuid = crypto.randomUUID();
+
+  const device = database.select("device", deviceUuid);
+
+  if (!device) return res.status(404).json({ error: "No device found" });
+
+  if (!dataToBeSigned)
+    return res.status(404).json({ error: "No data to be signed found" });
+
+  const { publicKey, privateKey, signatureCounter, algorithm } = device;
+
+  const keyPair = {
+    publicKey,
+    privateKey,
+  };
+
+  const signature =
+    algorithm === "ECC"
+      ? Buffer.from(signECC(dataToBeSigned, keyPair)).toString("base64")
+      : Buffer.from(signRSA(dataToBeSigned, keyPair)).toString("base64");
+
+  const lastSignatureBase64 =
+    device.lastSignature || Buffer.from(deviceUuid).toString("base64");
+
+  const signedData = `${signatureCounter}_${dataToBeSigned}_${lastSignatureBase64}`;
+
+  device.signatureCounter += 1;
+  device.lastSignature = signature;
+  //
+  console.log("device = ", device);
+  database.update("device", deviceUuid, device);
+
+  const transaction: ITransaction = {
+    uuid,
+    signature,
+    signedData,
+    deviceUuid,
+  };
+
+  database.insert("transaction", transaction);
+
+  const response: ITransaction = {
+    signature,
+    signedData,
+  };
+
+  res.json(response);
+});
+
+server.get("/transaction", async (req, res) => {
+  res.json(database.select("transaction"));
 });
 
 export default server;
